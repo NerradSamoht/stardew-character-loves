7118a8d1797a9359f7bdc466e38505db8f322468
nerradsamoht <darren.thomas5@talktalk.net>
2019-04-21 13:09:19 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import stardew from "./stardew.json";
+import Character from "./Character";
+import uniqid from "uniqid";
+
+const data = stardew.Characters;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: data,
+      showCharacter: false,
+      name: null,
+      birthday: null,
+      loves: null,
+      img: null
+    };
+  }
+
+  getCharacter = e => {
+    const name = e.target.getAttribute("data-name");
+    const character = this.state.characters.filter(item => item.name === name);
+    console.log(character);
+
+    if (character.length > 0) {
+      this.setState(
+        {
+          name: character[0].name,
+          birthday: character[0].birthday,
+          loves: character[0].loves,
+          img: character[0].img
+        },
+        this.showCharacter()
+      );
+    }
+  };
+
+  showCharacter = () => {
+    this.setState({
+      showCharacter: true
+    });
+  };
+
+  hideCharacter = () => {
+    this.setState({
+      showCharacter: false
+    });
+  };
+
   render() {
+    const characterList = this.state.characters.map(item => (
+      <button key={uniqid()} onClick={this.getCharacter} data-name={item.name}>
+        <img
+          src={item.img}
+          alt={item.name}
+          title={item.name}
+          data-name={item.name}
+        />
+      </button>
+    ));
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {!this.state.showCharacter ? (
+          <div>
+            <div className="container">
+              <div>
+                <h2>Select a character</h2>
+                <div className="character-list">{characterList}</div>
+              </div>
+            </div>
+            <footer>
+              <p>
+                All content and images from the{" "}
+                <a href="https://stardewvalleywiki.com/Stardew_Valley_Wiki">
+                  Official Stardew Valley Wiki
+                </a>
+                . <br />
+                Licenced under{" "}
+                <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/">
+                  Creative Commons Attribution-NonCommercial-ShareAlike
+                </a>
+                .
+              </p>
+            </footer>
+          </div>
+        ) : (
+          <div className="container">
+            <Character
+              hideCharacter={this.hideCharacter}
+              img={this.state.img}
+              name={this.state.name}
+              birthday={this.state.birthday}
+              loves={this.state.loves}
+            />
+          </div>
+        )}
       </div>
     );
   }
---
+++ src/Character.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import uniqid from "uniqid";
+
+class Character extends Component {
+  render() {
+    const loves = this.props.loves.map(item => {
+      const file = item.replace(/\s/g, "_");
+      const src = "/assets/" + file + ".png";
+      return (
+        <li key={uniqid()}>
+          <img src={src} alt="" />
+          {item}
+        </li>
+      );
+    });
+
+    return (
+      <div className="">
+        <button onClick={this.props.hideCharacter} className="close">
+          X
+        </button>
+        <img
+          width="80"
+          src={this.props.img}
+          className="profile-pic"
+          alt={this.props.name}
+        />
+        <p className="name">{this.props.name}</p>
+        <p className="birthday">Birthday: {this.props.birthday}</p>
+        <ul className="list">{loves}</ul>
+      </div>
+    );
+  }
+}
+
+export default Character;
