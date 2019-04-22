c6a27a6f2d276f0a5d9a7c1c2fcf5465823ca03a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-22 14:42:13 +0000

    Deploy NerradSamoht/stardew-character-loves to github.com/NerradSamoht/stardew-character-loves.git:gh-pages

---
+++ precache-manifest.f02047a62f2160f260f0db2d0100f835.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75d7f2bc580a7ff8ff9d",
+    "url": "/stardew-character-loves/static/css/main.af647344.chunk.css"
+  },
+  {
+    "revision": "75d7f2bc580a7ff8ff9d",
+    "url": "/stardew-character-loves/static/js/main.a728322d.chunk.js"
+  },
+  {
+    "revision": "2213ad5ded4180b6577e",
+    "url": "/stardew-character-loves/static/js/runtime~main.441975fd.js"
+  },
+  {
+    "revision": "59e630c65aca146c363e",
+    "url": "/stardew-character-loves/static/js/2.139b3c35.chunk.js"
+  },
+  {
+    "revision": "7337c3fd32f659727b23fd71c6759188",
+    "url": "/stardew-character-loves/index.html"
+  }
+];
